038ac2515c3c2b3eaf623748d0749c894b4feaa8
Andre Jaques <andrejaques.b@gmail.com>
2021-12-07 07:35:50 -0300

    add firewall to protect from some requests methods

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const cors = require('cors');
 const jwtAuth = require('./auth/validateJWT');
+const firewall = require('./middlewares/firewall');
 const users = require('./controllers/users')
 const register = require('./controllers/register');
 const login = require('./controllers/login');
 
 const router = express.Router({ mergeParams: true });
 
-router.use('/users', jwtAuth, users);
-router.use('/register', register);
-router.use('/login', login);
+router.use('/users', firewall, jwtAuth, users);
+router.use('/register', firewall, register);
+router.use('/login', firewall, login);
 
 module.exports = router;
